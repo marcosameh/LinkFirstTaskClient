2fe44c09305abfe6775124abeb0550c5ee486bcd
Marco Sameh <marcosameh678@gmail.com>
2024-11-05 16:24:57 +0200

    product list

--- src/components/products/products.component.ts
+++ src/components/products/products.component.ts
@@ -2,31 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../models/Product.model';
 import { PaginationFilter } from '../../models/paginationfilter.model';
-import {DataTablesModule} from "angular-datatables";
+import { DataTablesModule } from "angular-datatables";
 import { Subject } from 'rxjs';
 import { Config } from 'datatables.net';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css'],
   standalone: true,
-  imports:[DataTablesModule],
+  imports: [DataTablesModule, CommonModule,FormsModule],
 })
 export class ProductsComponent implements OnInit {
   products: Product[] = [];
   totalRecords: number = 0;
-  dtoptions: Config= {};
-  dtTrigger:Subject<any>=new Subject<any>();
-  constructor(private productService: ProductService) { }
+  dtoptions: Config = {};
+  dtTrigger: Subject<any> = new Subject<any>();
+
+  // Add an array to track quantities for each product
+  quantities: number[] = [];
+
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.loadProducts();
     this.dtoptions = {
       pagingType: 'full_numbers'
-     };
-    this.dtTrigger.next(null);
+    };
   }
 
   loadProducts(): void {
@@ -35,10 +39,20 @@ export class ProductsComponent implements OnInit {
       if (response.success) {
         this.products = response.data.data;
         this.totalRecords = response.data.totalRecords;
+        
+        // Initialize the quantities array with default values (1 for each product)
+        this.quantities = new Array(this.products.length).fill(1);
+        
+        this.dtTrigger.next(null);
       } else {
         console.error(response.errors);
       }
-      console.log(response);
     });
+  }
+
+  // Add to cart function accepting product and quantity
+  addToCart(product: Product, quantity: number): void {
+    console.log(`Adding ${quantity} of ${product.name} to cart`);
+    // Logic to add the product with the specified quantity to the cart
   }
 }
