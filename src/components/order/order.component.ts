677b121de3fa81b50687dcec6c77c3f3d7930988
Marco Sameh <marcosameh678@gmail.com>
2024-11-07 11:08:57 +0200

    updates

--- src/components/order/order.component.ts
+++ src/components/order/order.component.ts
@@ -4,17 +4,21 @@ import { Order } from '../../models/Order.model';
 import { OrderService } from '../../services/order.service';
 import { ToastrService } from 'ngx-toastr';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-order',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './order.component.html',
-  styleUrl: './order.component.css'
+  styleUrls: ['./order.component.css']
 })
 export class OrderComponent implements OnInit {
   orderId!: number;
   order!: Order;
+  customerName = '';
+  customerPhone = '';
+  customerAddress = '';
 
   constructor(
     private route: ActivatedRoute,
@@ -29,18 +33,40 @@ export class OrderComponent implements OnInit {
     });
   }
 
-
   LoadOrder(id: number): void {
     this.orderService.getOrder(id).subscribe({
       next: (res) => {
         if (res.success) {
-          this.order = res.data;
+          this.order = res.data;        
         } else {
           this.toastr.error('Failed to load order', res.errors.join(', '));
         }
       },
       error: (err) => {
-        this.toastr.error('An error occurred while fetching the order', err);     
+        this.toastr.error('An error occurred while fetching the order', err);
+      }
+    });
+  }
+
+  onSubmit(): void {
+    const submitOrderData = {
+      orderId: this.orderId,
+      customerName: this.customerName,
+      customerPhone: this.customerPhone,
+      customerAddress: this.customerAddress
+    };
+
+    this.orderService.submitOrder(submitOrderData).subscribe({
+      next: (res) => {
+        if (res.success) {
+          this.toastr.success('Order submitted successfully!', 'Success');
+        } else {
+          this.toastr.error('Failed to submit order', res.errors.join(', '));
+        }
+      },
+      error: (err) => {
+        this.toastr.error('An error occurred while submitting the order', 'Error');
+        console.error('Error submitting order:', err);
       }
     });
   }
